72279b7d688771d0c8da012f3a2291cdda0cacbf
aStudentAlways <aStudentAlways@users.noreply.github.com>
2021-09-27 16:47:19 +0500

    new file:   src/build.bat 	new file:   src/clean.bat

---
+++ src/build.bat
@@ -0,0 +1,6 @@
+
+
+set CTOOLS_PATH=C:\Program Files (x86)\Control Microsystems\CTools
+set PATH=C:\Program Files (x86)\Control Microsystems\CTools\Arm7\host\x86-win32\bin;%PATH%
+
+make TARGET=SCADAPack350 
---
+++ src/clean.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set CTOOLS_PATH=C:\Program Files (x86)\Control Microsystems\CTools
+set PATH=C:\Program Files (x86)\Control Microsystems\CTools\Arm7\host\x86-win32\bin;%PATH%
+
+make clean
